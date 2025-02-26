0e1a7eee35afacd4b0abe7c3403f68b872aef6b0
Brandon <brandontruong04@gmail.com>
2025-02-27 00:16:54 +1100

    Found qualifying regions - Appended first cell of each row

--- main.py
+++ main.py
@@ -9,4 +9,13 @@ soup = BeautifulSoup(page.content, "html.parser")
 # find participating teams
 qualified_teams_table = soup.find("table", class_="wikitable")
 
-print(qualified_teams_table.prettify())
+# finding team names and region
+qualified_teams_table_rows = qualified_teams_table.find_all('tr')[2:]
+
+qualified_teams_region =[]
+for row in qualified_teams_table_rows:
+    qualified_teams_region.append(row.find_all('td')[0].get_text())
+
+#print(qualified_teams_table.prettify())
+print(qualified_teams_region)
+#print(qualified_teams_table_rows[1])
