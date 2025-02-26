af4f21ed0b9e0e11e59fe82a0e5a995ee88b1868
Brandon <brandontruong04@gmail.com>
2025-02-27 01:02:31 +1100

    Found qualifying teams - Accounts for when a team has not yet qualified

--- main.py
+++ main.py
@@ -9,13 +9,35 @@ soup = BeautifulSoup(page.content, "html.parser")
 # find participating teams
 qualified_teams_table = soup.find("table", class_="wikitable")
 
-# finding team names and region
+# finding qualified regions
 qualified_teams_table_rows = qualified_teams_table.find_all('tr')[2:]
 
-qualified_teams_region =[]
+qualified_teams_region = []
 for row in qualified_teams_table_rows:
     qualified_teams_region.append(row.find_all('td')[0].get_text())
 
+#finding qualified teams
+qualified_teams = []
+for row in qualified_teams_table_rows:
+    cells = row.find_all('td')
+    team_cell = cells[1]  # The second <td> (team column)
+    team_span = team_cell.find('span', class_="team")
+
+    if team_span:
+        teamname_span = team_span.find('span', class_="teamname")
+        if teamname_span:
+            team_link = teamname_span.find('a')
+            if team_link:
+                qualified_teams.append(team_link.get_text())
+            else:
+                qualified_teams.append("tbd")
+        else:
+            qualified_teams.append("tbd")
+    else:
+        qualified_teams.append("tbd")
+    
+
 #print(qualified_teams_table.prettify())
 print(qualified_teams_region)
+print(qualified_teams)
 #print(qualified_teams_table_rows[1])
