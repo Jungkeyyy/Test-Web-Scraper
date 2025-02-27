5da3744a01ef04f26e3d35ec08b5514678434aae
Brandon <brandontruong04@gmail.com>
2025-02-27 18:11:43 +1100

    Going back to First Stand 2025

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.nba.com/schedule"
+url = "https://lol.fandom.com/wiki/2025_First_Stand"
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, "html.parser")
-    
-# schedule
-schedule = soup.find_all("div")
 
-print(soup)
+# find participating teams
+qualified_teams_table = soup.find("table", class_="wikitable")
+
+# finding qualified regions
+qualified_teams_table_rows = qualified_teams_table.find_all('tr')[2:]
+
+qualified_teams_region = []
+for row in qualified_teams_table_rows:
+    qualified_teams_region.append(row.find_all('td')[0].find_all(text=True, recursive=False)[-1].get_text(strip=True))
+
+#finding qualified teams
+qualified_teams = []
+for row in qualified_teams_table_rows:
+    cells = row.find_all('td')
+    team_cell = cells[1]  
+    team_span = team_cell.find('span', class_="team")
+
+    if team_span:
+        teamname_span = team_span.find('span', class_="teamname")
+        if teamname_span:
+            team_link = teamname_span.find('a')
+            if team_link:
+                qualified_teams.append(team_link.get_text())
+            else:
+                qualified_teams.append("tbd")
+        else:
+            qualified_teams.append("tbd")
+    else:
+        qualified_teams.append("tbd")
+
+
+#print(qualified_teams_table.prettify())
+print(qualified_teams_region)
+print(qualified_teams)
